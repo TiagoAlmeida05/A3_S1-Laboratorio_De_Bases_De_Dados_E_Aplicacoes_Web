cbc2c5886f15c67a0f43aec8b9d0b97243044fdf
Ana Beatriz Pinto <anabspinto@outlook.com>
2025-11-20 05:17:11 +0000

    Delete card-related components and add bootstrap CSS test

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,6 +6,7 @@
         <meta name="csrf-token" content="{{ csrf_token() }}">
 
         <title>@yield('title', config('app.name', 'Laravel'))</title>
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
 
         <!-- Styles -->
         <link rel="stylesheet" href="{{ asset('css/milligram.css') }}">
@@ -17,6 +18,7 @@
         @stack('scripts')
     </head>
     <body>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script>
         <main>
             <header>
                 <h1>HireUp!</h1>
--- resources/views/partials/job_posting.blade.php
+++ resources/views/partials/job_posting.blade.php
@@ -1,12 +1,18 @@
-<article class="job-posting" data-id="{{ $job_posting->id }}">
-    <header>
-        <h2>
-            {{-- Link to the detailed job posting page --}}
-            <a href="{{ route('job_postings.show', $job_posting) }}">
-                {{ $job_posting->title }}
-            </a>
-        </h2>
-        <p>Deadline: {{ $job_posting->deadline ?? 'N/A' }}</p>
-        <p>Description: {{ $job_posting->description }}</p>
-    </header>
+<article class="job-posting mb-3" data-id="{{ $job_posting->id }}">
+    <div class="card border-light mb-3 container d-flex justify-content-center" style="max-width: 150rem; ">
+        <div class="card-header">
+            <h3>
+                {{-- Link to the detailed job posting page --}}
+                <a href="{{ route('job_postings.show', $job_posting) }}">
+                    {{ $job_posting->title }}
+                </a>
+            </h3>
+        </div>
+        <div class="card-body">
+            <p class="card-text">
+                <p><strong>Deadline</strong>: {{ $job_posting->deadline ?? 'N/A' }}</p>
+                <p><strong>Description</strong>: {{ $job_posting->description }}</p>
+            </p>
+        </div>
+    </div>
 </article>
