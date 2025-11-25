c6f5991cd61e6173632ed79e610485ea06a57d06
Inês Rocha de Oliveira <up202305418@edu.fe.up.pt>
2025-11-25 22:52:16 +0000

    removed ===== from routes

--- routes/web.php
+++ routes/web.php
@@ -81,7 +81,6 @@ Route::middleware(['auth', 'admin'])->group(function () {
     Route::patch('/admin/content/{id}/solve', [AdminController::class, 'solveReport'])->name('admin.reports.solve');
     Route::get('/admin/pages', [AdminController::class, 'editPages'])->name('admin.pages');
     Route::patch('/admin/content/{id}/reopen', [AdminController::class, 'reopenReport'])->name('admin.reports.reopen');
-=======
 });
 
 Route::get('/recruiter-dashboard', function () {
