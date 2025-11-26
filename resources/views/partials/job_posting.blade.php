cffcb2f323e77605dfbf756fb8d09f88077f29a8
Ana Beatriz Pinto <anabspinto@outlook.com>
2025-11-26 20:38:36 +0000

    Correct job postings display

--- resources/views/partials/job_posting.blade.php
+++ resources/views/partials/job_posting.blade.php
@@ -1,6 +1,6 @@
-<article class="job-posting mb-3" data-id="{{ $job_posting->id }}">
-    <div class="card border-light mb-3 container d-flex justify-content-center" style="max-width: 150rem; ">
-        <div class="card-header">
+<article class="job-posting-partial mb-3" data-id="{{ $job_posting->id }}">
+    <div class="jp-container border-light mb-3 container d-flex justify-content-center" style="max-width: 150rem; ">
+        <div class="jp-header">
             <h3>
                 {{-- Link to the detailed job posting page --}}
                 <a href="{{ route('job_postings.show', $job_posting) }}">
@@ -8,9 +8,9 @@
                 </a>
             </h3>
         </div>
-        <div class="card-body">
-            <p class="card-text">
-                <p><strong>Deadline</strong>: {{ $job_posting->deadline ?? 'N/A' }}</p>
+        <div class="jp-body">
+            <p class="jp-content">
+                <p><strong>Deadline:</strong>: {{ \Carbon\Carbon::parse($job_posting->deadline)->format('d-m-Y') }}</p>
                 <p><strong>Description</strong>: {{ $job_posting->description }}</p>
             </p>
         </div>
