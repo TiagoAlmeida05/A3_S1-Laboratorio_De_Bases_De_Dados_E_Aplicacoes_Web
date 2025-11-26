95b477ba0dd5a7e838f6499c6c5913235fba4949
Inês Rocha de Oliveira <up202305418@edu.fe.up.pt>
2025-11-26 19:42:28 +0000

    mostra ainda mais texto

--- resources/views/pages/job_posting.blade.php
+++ resources/views/pages/job_posting.blade.php
@@ -11,6 +11,12 @@
         @endif
     @endauth
 
+    @if(session('success'))
+        <div class="alert alert-success">
+            {{ session('success') }}
+        </div>
+    @endif
+    
     @if(session('error'))
         <div class="alert alert-danger">
             {{ session('error') }}
