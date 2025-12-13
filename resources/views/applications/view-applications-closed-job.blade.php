f2c719a57d30a99fcb35b412cf4fc0105d5283ad
Ana Beatriz Pinto <anabspinto@outlook.com>
2025-12-13 18:52:29 +0000

    Add 'return to my dashboard' button on view applications of closed job

--- resources/views/applications/view-applications-closed-job.blade.php
+++ resources/views/applications/view-applications-closed-job.blade.php
@@ -14,44 +14,41 @@
     @if($applications->isEmpty())
         <p>No applications were submitted for this job posting.</p>
         <a href="{{ route('recruiter-dashboard.index') }}" class="button btn btn-primary" style="background-color: #1c4eb1eb; padding: 0.5rem 0.4rem;">Return to my dashboard</a>
-    @else
-        <form id="applicant-selection-form" style="padding: 3rem; width: 40rem; border-color: #4856e7ff;" method="POST" action="{{ route('job_postings.submit-application-selection', $job_posting->id) }}" id="application-selection-form">
-            @csrf
-            
-            <table>
-                <thead>
+    @else            
+        <table>
+            <thead>
+                <tr>
+                    <th>Applicant name</th>
+                    <th>Application date</th>
+                    <th>Cover letter?</th>
+                    <th>Recommendation letter?</th>
+                    <th>Status</th>
+                </tr>
+            </thead>
+            <tbody>
+                @foreach($applications as $application)
                     <tr>
-                        <th>Applicant name</th>
-                        <th>Application date</th>
-                        <th>Cover letter?</th>
-                        <th>Recommendation letter?</th>
-                        <th>Status</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    @foreach($applications as $application)
-                        <tr>
-                            <td><a href="{{ route('jobseeker.profile', $application->jobSeeker->registered_user_id) }}">{{ $application->jobSeeker->registeredUser->name }}</a></td>
-                            <td>{{ $application->date->format('d-m-Y') }}</td>
-                            <td>{{ $application->cover_letter ? 'Yes' : 'No' }}</td>
-                            <td>{{ $application->recommendation_letter ? 'Yes' : 'No' }}</td>
-                            <td>
-                                @if($application->evaluated)
-                                    @if($application->accepted)
-                                        <span>Accepted</span>
-                                    @else
-                                        <span>Rejected</span>
-                                    @endif
+                        <td><a href="{{ route('jobseeker.profile', $application->jobSeeker->registered_user_id) }}">{{ $application->jobSeeker->registeredUser->name }}</a></td>
+                        <td>{{ $application->date->format('d-m-Y') }}</td>
+                        <td>{{ $application->cover_letter ? 'Yes' : 'No' }}</td>
+                        <td>{{ $application->recommendation_letter ? 'Yes' : 'No' }}</td>
+                        <td>
+                            @if($application->evaluated)
+                                @if($application->accepted)
+                                    <span>Accepted</span>
                                 @else
-                                    <span>Pending evaluation</span>
+                                    <span>Rejected</span>
                                 @endif
-                            </td>
-                            <td><a href="{{ route('job_postings.view-application-closed-job', $application->id) }}" class="button btn btn-primary" style="background-color: #1c4eb1eb; padding: 0.5rem 0.4rem;">See full application</a></td>
-                        </tr>
-                    @endforeach
-                </tbody>
-            </table>
-        </form>
+                            @else
+                                <span>Pending evaluation</span>
+                            @endif
+                        </td>
+                        <td><a href="{{ route('job_postings.view-application-closed-job', $application->id) }}" class="button btn btn-primary" style="background-color: #1c4eb1eb; padding: 0.5rem 0.4rem;">See full application</a></td>
+                    </tr>
+                @endforeach
+            </tbody>
+        </table>
+        <a href="{{ route('recruiter-dashboard.index') }}" class="button btn btn-primary" style="background-color: #1c4eb1eb; padding: 0.5rem 0.4rem;">Return to my dashboard</a>
     @endif
 </section>
 
