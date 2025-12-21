9baa6c05779fb74840633eb7bb9160e29c4aeea6
Inês Rocha de Oliveira <up202305418@edu.fe.up.pt>
2025-12-21 18:59:31 +0000

    resolver issue on bookmarks - company name correctly displayed

--- resources/views/jobseeker/bookmarks.blade.php
+++ resources/views/jobseeker/bookmarks.blade.php
@@ -22,9 +22,10 @@
                         </h5>
 
                         <p>
-                            {{ $job->company->name ?? 'Unknown Company' }}
-                            · {{ $job->city->name ?? 'Unknown City' }}
+                            {{ $job->recruiter?->department?->company?->name ?? 'Unknown Company' }}
+                            · {{ $job->city?->name ?? 'Unknown City' }}
                         </p>
+
 
                         <p>
                             <strong>Deadline:</strong>
