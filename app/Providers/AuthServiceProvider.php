c597ab7f8964e1a45ef50bf03b16f017f7807fa8
Inês Rocha de Oliveira <up202305418@edu.fe.up.pt>
2025-12-22 09:57:19 +0000

    policies in view profile working

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -11,6 +11,7 @@ class AuthServiceProvider extends ServiceProvider
 
     protected $policies = [
         Company::class => CompanyPolicy::class,
+        JobSeeker::class => JobSeekerPolicy::class,
     ];
 
     public function boot()
